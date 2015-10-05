d29f0e2f4b3797f96bda11a331ecc2f20c15df1f
Kunio Sawatani <devel@fathens.org>
2015-10-05 17:31:17 +0900

    JavaScript を Crashlytics と Answers に分離

--- www/crashlytics.js
+++ www/crashlytics.js
@@ -27,44 +27,5 @@ module.exports = {
 		},
 		crash: function(message) {
 			exec(function() {}, function(error) {}, "crash", [message]);
-		},
-		eventPurchase: function(map) {
-			exec(function() {}, function(error) {}, "eventPurchase", [map]);
-		},
-		eventAddToCart: function(map) {
-			exec(function() {}, function(error) {}, "eventAddToCart", [map]);
-		},
-		eventStartCheckout: function(map) {
-			exec(function() {}, function(error) {}, "eventStartCheckout", [map]);
-		},
-		eventContentView: function(map) {
-			exec(function() {}, function(error) {}, "eventContentView", [map]);
-		},
-		eventSearch: function(map) {
-			exec(function() {}, function(error) {}, "eventSearch", [map]);
-		},
-		eventShare: function(map) {
-			exec(function() {}, function(error) {}, "eventShare", [map]);
-		},
-		eventRating: function(map) {
-			exec(function() {}, function(error) {}, "eventRating", [map]);
-		},
-		eventSignUp: function(map) {
-			exec(function() {}, function(error) {}, "eventSignUp", [map]);
-		},
-		eventLogin: function(map) {
-			exec(function() {}, function(error) {}, "eventLogin", [map]);
-		},
-		eventInvite: function(map) {
-			exec(function() {}, function(error) {}, "eventInvite", [map]);
-		},
-		eventLevelStart: function(map) {
-			exec(function() {}, function(error) {}, "eventLevelStart", [map]);
-		},
-		eventLevelEnd: function(map) {
-			exec(function() {}, function(error) {}, "eventLevelEnd", [map]);
-		},
-		eventCustom: function(map) {
-			exec(function() {}, function(error) {}, "eventCustom", [map]);
 		}
 }
