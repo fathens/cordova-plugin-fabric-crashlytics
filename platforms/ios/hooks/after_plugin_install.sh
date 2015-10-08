08a22c3f3617e948466c1db9973c7f5b0c0bac80
Kunio Sawatani <devel@fathens.org>
2015-10-08 17:29:43 +0900

    iOS の hooks で Bridging-Header を設定

--- platforms/ios/hooks/after_plugin_install.sh
+++ platforms/ios/hooks/after_plugin_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -eu
 
+plugin_id=$1
+
 prefix="$(cd $(dirname $0); pwd)/after_plugin_install"
 
 ${prefix}-podfile.sh
-${prefix}-fix_xcodeproj.sh
+${prefix}-fix_xcodeproj.sh "$plugin_id"
 ${prefix}-initialize.sh
 ${prefix}-api_key.sh
