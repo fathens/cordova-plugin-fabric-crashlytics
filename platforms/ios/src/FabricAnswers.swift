0005803260bb5e2a7c617793a1f8c63e13732d0e
Kunio Sawatani <devel@fathens.org>
2015-10-14 13:47:30 +0900

    Swift での全メソッドを実装

--- platforms/ios/src/FabricAnswers.swift
+++ platforms/ios/src/FabricAnswers.swift
@@ -2,21 +2,126 @@ import Foundation
 
 @objc(FabricAnswers)
 class FabricAnswers: CDVPlugin {
-    func eventLogin(command: CDVInvokedUrlCommand) {
-        func event() -> (String?, NSNumber?, [String: AnyObject]?) {
-            if let v = command.arguments.first {
-                if (v is NSDictionary) {
-                    let method = v.stringForKey("method")
-                    let success = v.boolForKey("success") ? 1 : 0
-                    let custom = v.dictionaryForKey("custom")
-                    return (method, success, custom)
-                }
-            }
-            return (nil, nil, nil)
-        }
-        let (method, success, attrs) = event()
-        Answers.logLoginWithMethod(method, success: success, customAttributes: attrs)
+    private func frame(command: CDVInvokedUrlCommand, _ proc: (Dictionary<String, AnyObject>?, Dictionary<String, AnyObject>?) -> Void) {
+        let dict = command.arguments.first.map {            ($0 is NSDictionary)  ? $0 as! Dictionary<String, AnyObject> :  [:]        }
+        let custom = dict?["custom"] as? Dictionary<String, AnyObject>
         
+        proc(dict, custom)
         commandDelegate!.sendPluginResult(CDVPluginResult(status: CDVCommandStatus_OK), callbackId: command.callbackId)
+    }
+    
+    func eventPurchase(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let price = dict?["itemPrice"] as? NSDecimalNumber
+            let currency = dict?["currency"] as? String
+            let success = (dict?["success"] as? Bool).map { $0 ? 1 : 0 }
+            let name = dict?["itemName"] as? String
+            let type = dict?["itemType"] as? String
+            let id = dict?["itemId"] as? String
+            Answers.logPurchaseWithPrice(price, currency: currency, success: success, itemName: name, itemType: type, itemId: id, customAttributes: custom)
+        }
+    }
+    
+    func eventAddToCart(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let price = dict?["itemPrice"] as? NSDecimalNumber
+            let currency = dict?["currency"] as? String
+            let name = dict?["itemName"] as? String
+            let type = dict?["itemType"] as? String
+            let id = dict?["itemId"] as? String
+            Answers.logAddToCartWithPrice(price, currency: currency, itemName: name, itemType: type, itemId: id, customAttributes: custom)
+        }
+    }
+    
+    func eventStartCheckout(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let price = dict?["totalPrice"] as? NSDecimalNumber
+            let currency = dict?["currency"] as? String
+            let count = dict?["itemCount"] as? NSNumber
+            Answers.logStartCheckoutWithPrice(price, currency: currency, itemCount: count, customAttributes: custom)
+        }
+    }
+    
+    func eventContentView(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let name = dict?["contentName"] as? String
+            let type = dict?["contentType"] as? String
+            let id = dict?["contentId"] as? String
+            Answers.logContentViewWithName(name, contentType: type, contentId: id, customAttributes: custom)
+        }
+    }
+    
+    func eventSearch(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let query = dict?["query"] as? String
+            Answers.logSearchWithQuery(query, customAttributes: custom)
+        }
+    }
+    
+    func eventShare(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let method = dict?["method"] as? String
+            let name = dict?["contentName"] as? String
+            let type = dict?["contentType"] as? String
+            let id = dict?["contentId"] as? String
+            Answers.logShareWithMethod(method, contentName: name, contentType: type, contentId: id, customAttributes: custom)
+        }
+    }
+    
+    func eventRating(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let rating = dict?["rating"] as? NSNumber
+            let name = dict?["contentName"] as? String
+            let type = dict?["contentType"] as? String
+            let id = dict?["contentId"] as? String
+            Answers.logRating(rating, contentName: name, contentType: type, contentId: id, customAttributes: custom)
+        }
+    }
+    
+    func eventSignUp(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let method = dict?["method"] as? String
+            let success = (dict?["success"] as? Bool).map { $0 ? 1 : 0 }
+            Answers.logSignUpWithMethod(method, success: success, customAttributes: custom)
+        }
+    }
+    
+    func eventLogin(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let method = dict?["method"] as? String
+            let success = (dict?["success"] as? Bool).map { $0 ? 1 : 0 }
+            Answers.logLoginWithMethod(method, success: success, customAttributes: custom)
+        }
+    }
+    
+    func eventInvite(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let method = dict?["method"] as? String
+            Answers.logInviteWithMethod(method, customAttributes: custom)
+        }
+    }
+    
+    func eventLevelStart(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let name = dict?["levelName"] as? String
+            Answers.logLevelStart(name, customAttributes: custom)
+        }
+    }
+    
+    func eventLevelEnd(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let name = dict?["levelName"] as? String
+            let score = dict?["score"] as? NSNumber
+            let success = (dict?["success"] as? Bool).map { $0 ? 1 : 0 }
+            Answers.logLevelEnd(name, score: score, success: success, customAttributes: custom)
+        }
+    }
+    
+    func eventCustom(command: CDVInvokedUrlCommand) {
+        frame(command) { dict, custom in
+            let name = dict?["name"] as? String
+            let attrs = dict?["attributes"] as? Dictionary<String, AnyObject>
+            Answers.logCustomEventWithName(name == nil ? "NoName": name!, customAttributes: attrs)
+        }
     }
 }
--- platforms/ios/src/FabricCrashlytics.swift
+++ platforms/ios/src/FabricCrashlytics.swift
@@ -2,31 +2,87 @@ import Foundation
 
 @objc(FabricCrashlytics)
 class FabricCrashlytics: CDVPlugin {
-    private func logmsg(command: CDVInvokedUrlCommand) -> String? {
-        return command.arguments.first.map {
-            let msg = String($0)
-            CLSLogv("%@", getVaList([msg]))
-            return msg
+    private func frame(command: CDVInvokedUrlCommand, _ proc: () -> Void) {
+        proc()
+        commandDelegate!.sendPluginResult(CDVPluginResult(status: CDVCommandStatus_OK), callbackId: command.callbackId)
+    }
+    
+    private func logmsg(command: CDVInvokedUrlCommand, _ proc: () -> Void = { }) {
+        frame(command) {
+            if let v = command.arguments.first {
+                let msg = String(v)
+                CLSLogv("%@", getVaList([msg]))
+            }
+            proc()
         }
     }
     
     func log(command: CDVInvokedUrlCommand) {
         logmsg(command)
-        
-        commandDelegate!.sendPluginResult(CDVPluginResult(status: CDVCommandStatus_OK), callbackId: command.callbackId)
     }
     
     func logException(command: CDVInvokedUrlCommand) {
-        logmsg(command)
-        Crashlytics.sharedInstance().throwException()
-        
-        commandDelegate!.sendPluginResult(CDVPluginResult(status: CDVCommandStatus_OK), callbackId: command.callbackId)
+        logmsg(command) {
+            Crashlytics.sharedInstance().throwException()
+        }
     }
     
     func crash(command: CDVInvokedUrlCommand) {
-        logmsg(command)
-        Crashlytics.sharedInstance().crash()
-        
-        commandDelegate!.sendPluginResult(CDVPluginResult(status: CDVCommandStatus_OK), callbackId: command.callbackId)
+        logmsg(command) {
+            Crashlytics.sharedInstance().crash()
+        }
+    }
+    
+    func setBool(command: CDVInvokedUrlCommand) {
+        let key = command.arguments[0] as! String
+        let value = command.arguments[1] as! Bool
+        frame(command) {
+            Crashlytics.sharedInstance().setBoolValue(value, forKey: key)
+        }
+    }
+    
+    func setDouble(command: CDVInvokedUrlCommand) {
+        let key = command.arguments[0] as! String
+        let value = command.arguments[1] as! Float64
+        frame(command) {
+            Crashlytics.sharedInstance().setObjectValue(value, forKey: key)
+        }
+    }
+    
+    func setFloat(command: CDVInvokedUrlCommand) {
+        let key = command.arguments[0] as! String
+        let value = command.arguments[1] as! Float32
+        frame(command) {
+            Crashlytics.sharedInstance().setFloatValue(value, forKey: key)
+        }
+    }
+    
+    func setInt(command: CDVInvokedUrlCommand) {
+        let key = command.arguments[0] as! String
+        let value = command.arguments[1] as! Int32
+        frame(command) {
+            Crashlytics.sharedInstance().setIntValue(value, forKey: key)
+        }
+    }
+    
+    func setUserIdentifier(command: CDVInvokedUrlCommand) {
+        let value = command.arguments[0] as! String
+        frame(command) {
+            Crashlytics.sharedInstance().setUserIdentifier(value)
+        }
+    }
+    
+    func setUserName(command: CDVInvokedUrlCommand) {
+        let value = command.arguments[0] as! String
+        frame(command) {
+            Crashlytics.sharedInstance().setUserName(value)
+        }
+    }
+    
+    func setUserEmail(command: CDVInvokedUrlCommand) {
+        let value = command.arguments[0] as! String
+        frame(command) {
+            Crashlytics.sharedInstance().setUserEmail(value)
+        }
     }
 }
