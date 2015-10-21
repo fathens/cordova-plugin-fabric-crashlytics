ea04e86e936c04a2cce7bfd06c14674b7f72e5e8
Kunio Sawatani <devel@fathens.org>
2015-10-21 14:45:15 +0900

    Ruby での環境変数の取り方を修正

--- platforms/ios/hooks/after_plugin_install-fix_xcodeproj.rb
+++ platforms/ios/hooks/after_plugin_install-fix_xcodeproj.rb
@@ -9,7 +9,7 @@ project = Xcodeproj::Project.open proj
 
 project.targets.each do |target|
     phase = target.new_shell_script_build_phase "Fabric"
-    phase.shell_script = "./Pods/Fabric/Fabric.framework/run $FABRIC_API_KEY $FABRIC_BUILD_SECRET"
+    phase.shell_script = "./Pods/Fabric/Fabric.framework/run #{ENV['FABRIC_API_KEY']} #{ENV['FABRIC_BUILD_SECRET']}"
 end
 
 project.save
