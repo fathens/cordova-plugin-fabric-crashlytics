085ba76145b0f5195bae5e31781080ff4743dd43
Kunio Sawatani <devel@fathens.org>
2015-11-12 15:09:20 +0900

    Fabric Subission がすでにある場合に対応

--- platforms/ios/hooks/after_prepare.rb
+++ platforms/ios/hooks/after_prepare.rb
@@ -6,9 +6,18 @@ Dir.chdir Pathname('platforms').join('ios').realpath
 
 project = Xcodeproj::Project.open(Pathname.glob('*.xcodeproj')[0])
 
+title = 'Fabric Submission'
 project.targets.each do |target|
-  phase = target.new_shell_script_build_phase('Fabric')
-  phase.shell_script = "./Pods/Fabric/Fabric.framework/run #{ENV['FABRIC_API_KEY']} #{ENV['FABRIC_BUILD_SECRET']}"
+  found = target.shell_script_build_phases.find do |phase|
+    phase.name == title
+  end
+  if found != nil then
+    puts "#{project}: already added '#{title}'"
+  else
+    phase = target.new_shell_script_build_phase(title)
+    puts "Adding '#{phase}' to #{target}: #{project}"
+    phase.shell_script = "./Pods/Fabric/Fabric.framework/run #{ENV['FABRIC_API_KEY']} #{ENV['FABRIC_BUILD_SECRET']}"
+  end
 end
 
 project.save
