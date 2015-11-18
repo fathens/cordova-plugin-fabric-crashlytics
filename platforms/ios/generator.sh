bd0fd2657f0ccb0a9dd5822ba83ca81b5a4bbc05
Kunio Sawatani <devel@fathens.org>
2015-11-18 10:32:15 +0900

    Podfile に "use_frameworks!" の記述があれば Bridging-Header は不要

--- platforms/ios/generator.sh
+++ platforms/ios/generator.sh
@@ -31,13 +31,11 @@ project.recreate_user_schemes
 group = project.new_group "Sources"
 
 sources = Dir.glob("src/*.swift").map { |path| group.new_file(path) }
-header = Dir.glob("src/*.h").map { |path| group.new_file(path) }.first
 
 target.add_file_references(sources)
 
 build_settings(project,
-  "ENABLE_BITCODE" => "NO",
-  "SWIFT_OBJC_BRIDGING_HEADER" => header.path
+  "ENABLE_BITCODE" => "NO"
 )
 
 project.save
@@ -48,6 +46,7 @@ echo "#### pod install"
 
 cat <<EOF > Podfile
 platform :ios, "8.0"
+use_frameworks!
 
 pod "Cordova"
 pod "Fabric"
