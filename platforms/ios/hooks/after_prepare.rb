0251ee156c636d3c731fe0bfe2a332660fcf2c14
Kunio Sawatani <devel@fathens.org>
2015-11-21 17:30:59 +0900

    Fabric/run にパスを変更

--- platforms/ios/hooks/after_prepare.rb
+++ platforms/ios/hooks/after_prepare.rb
@@ -16,7 +16,7 @@ project.targets.each do |target|
   else
     phase = target.new_shell_script_build_phase(title)
     puts "Adding '#{phase}' to #{target}: #{project}"
-    phase.shell_script = "./Pods/Fabric/Fabric.framework/run #{ENV['FABRIC_API_KEY']} #{ENV['FABRIC_BUILD_SECRET']}"
+    phase.shell_script = "./Pods/Fabric/run #{ENV['FABRIC_API_KEY']} #{ENV['FABRIC_BUILD_SECRET']}"
   end
 end
 
