ab1f85b2122f346fb299858eeade45e3e81045d0
Kunio Sawatani <devel@fathens.org>
2016-01-28 20:04:22 +0900

    console へのログは削除しておく

--- www/answers.js
+++ www/answers.js
@@ -1,7 +1,6 @@
 var cordova = require("cordova/exec");
 
 function exec(name, map) {
-	console.log("Calling FabricAnswersPlugin." + name + ": " + map);
 	cordova(function() {}, function(error) {
 		alert("Error on " + name + "\n" + error);
 	}, "FabricAnswersPlugin", name, [map]);
