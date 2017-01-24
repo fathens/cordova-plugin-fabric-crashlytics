97d3892884b38569e8a91d67ababcf353d2a3fb7
Kunio Sawatani <devel@fathens.org>
2017-01-24 23:38:42 +0900

    js-module でのループは関数に分離

--- www/crashlytics.js
+++ www/crashlytics.js
@@ -4,7 +4,17 @@ var pluginName = "FabricCrashlyticsPlugin";
 
 var obj = {};
 
-[
+function add_fun(methodName) {
+    obj[methodName] = function() {
+        var resolve = arguments[0];
+        var reject = arguments[1];
+        var args = Array.prototype.slice.call(arguments, 2);
+        console.log("Calling cordova plugin: " + pluginName + "." + methodName);
+        return cordova(resolve, reject, pluginName, methodName, args);
+    }
+}
+
+var names = [
 "log",
 "setBool",
 "setDouble",
@@ -14,14 +24,10 @@ var obj = {};
 "setUserName",
 "logException",
 "crash"
-].forEach((methodName) => {
-    obj[methodName] = function() {
-        var resolve = arguments[0];
-        var reject = arguments[1];
-        var args = Array.prototype.slice.call(arguments, 2);
-        console.log("Calling cordova plugin: " + pluginName + "." + methodName);
-        return cordova(resolve, reject, pluginName, methodName, args);
-    };
-});
+];
+
+for (var i = 0; i < names.length; i++) {
+    add_fun(names[i]);
+}
 
 module.exports = obj;
