93cdd2f5c9006c803227890f958a770c00013734
Kunio Sawatani <devel@fathens.org>
2017-01-24 23:13:38 +0900

    js-module からもログ出力

--- www/crashlytics.js
+++ www/crashlytics.js
@@ -22,6 +22,7 @@ for (var i = 0; i < names.length; i++) {
         var resolve = arguments[0];
         var reject = arguments[1];
         var args = Array.prototype.slice.call(arguments, 2);
+        console.log("Calling cordova plugin: " + pluginName + "." + methodName);
         return cordova(resolve, reject, pluginName, methodName, args);
     };
 }
