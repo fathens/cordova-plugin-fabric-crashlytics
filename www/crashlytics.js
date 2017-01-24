9b6553e8b597c95dbe84807e6fe08373749014fe
Kunio Sawatani <devel@fathens.org>
2017-01-24 23:31:02 +0900

    js-module でも Promise を使う

--- www/crashlytics.js
+++ www/crashlytics.js
@@ -6,11 +6,10 @@ var obj = {};
 
 function add_fun(methodName) {
     obj[methodName] = function() {
-        var resolve = arguments[0];
-        var reject = arguments[1];
-        var args = Array.prototype.slice.call(arguments, 2);
-        console.log("Calling cordova plugin: " + pluginName + "." + methodName);
-        return cordova(resolve, reject, pluginName, methodName, args);
+        var args = Array.prototype.slice.call(arguments, 0);
+        return new Promise(function(resolve, reject) {
+            cordova(resolve, reject, pluginName, methodName, args);
+        });
     }
 }
 
