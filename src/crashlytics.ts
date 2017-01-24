3d7c50de80ade79a2004de0b20e77ed75bdb9b03
Kunio SAWATANI <fsawa@fathens.org>
2017-01-24 02:53:09 +0000

    hasPlugin を都度呼び出すように変更
    モジュールの初期化時にプラグインがない可能性があるので、呼ばれた時にチェックするように変更します。

--- src/crashlytics.ts
+++ src/crashlytics.ts
@@ -8,7 +8,9 @@ const logger = new Logger("Crashlytics");
 function isDef(typedec) {
     return !_.isEqual(typedec, 'undefined');
 }
-const hasPlugin = isDef(typeof plugin) && isDef(typeof plugin.Fabric) && isDef(typeof plugin.Fabric.Crashlytics);
+function hasPlugin(): boolean {
+    return isDef(typeof plugin) && isDef(typeof plugin.Fabric) && isDef(typeof plugin.Fabric.Crashlytics);
+}
 
 export interface CrashlyticsClient {
     log(msg: string): Promise<void>;
@@ -23,82 +25,84 @@ export interface CrashlyticsClient {
     setUserEmail(value: string): Promise<void>;
 }
 
-const client: CrashlyticsClient = hasPlugin ? plugin.Fabric.Crashlytics : null;
-
 export class Crashlytics {
+    private static get client(): CrashlyticsClient {
+        return hasPlugin() ? plugin.Fabric.Crashlytics : null;
+    }
+
     static async log(msg: string): Promise<void> {
-        if (client) {
-            return client.log(msg);
+        if (Crashlytics.client) {
+            return Crashlytics.client.log(msg);
         }
     }
 
     static async logException(msg: string): Promise<void> {
-        if (client) {
-            return client.logException(msg);
+        if (Crashlytics.client) {
+            return Crashlytics.client.logException(msg);
         } else {
             logger.warn(() => `logException: ${msg}`);
         }
     }
 
     static async crash(msg: string): Promise<void> {
-        if (client) {
-            return client.crash(msg);
+        if (Crashlytics.client) {
+            return Crashlytics.client.crash(msg);
         } else {
             logger.fatal(() => `crash: ${msg}`);
         }
     }
 
     static async setBool(key: string, value: boolean): Promise<void> {
-        if (client) {
-            return client.setBool(key, value);
+        if (Crashlytics.client) {
+            return Crashlytics.client.setBool(key, value);
         } else {
             logger.info(() => `No Fabric! setBool ${key} = ${value}`);
         }
     }
 
     static async setDouble(key: string, value: number): Promise<void> {
-        if (client) {
-            return client.setDouble(key, value);
+        if (Crashlytics.client) {
+            return Crashlytics.client.setDouble(key, value);
         } else {
             logger.info(() => `No Fabric! setDouble ${key} = ${value}`);
         }
     }
 
     static async setFloat(key: string, value: number): Promise<void> {
-        if (client) {
-            return client.setFloat(key, value);
+        if (Crashlytics.client) {
+            return Crashlytics.client.setFloat(key, value);
         } else {
             logger.info(() => `No Fabric! setFloat ${key} = ${value}`);
         }
     }
 
     static async setInt(key: string, value: number): Promise<void> {
-        if (client) {
-            return client.setInt(key, value);
+        if (Crashlytics.client) {
+            return Crashlytics.client.setInt(key, value);
         } else {
             logger.info(() => `No Fabric! setInt ${key} = ${value}`);
         }
     }
 
     static async setUserIdentifier(value: string): Promise<void> {
-        if (client) {
-            return client.setUserIdentifier(value);
+        if (Crashlytics.client) {
+            return Crashlytics.client.setUserIdentifier(value);
         } else {
             logger.info(() => `No Fabric! set userIdentifier = ${value}`);
         }
     }
 
     static async setUserName(value: string): Promise<void> {
-        if (client) {
-            return client.setUserName(value);
+        if (Crashlytics.client) {
+            return Crashlytics.client.setUserName(value);
         } else {
             logger.info(() => `No Fabric! set userNmae = ${value}`);
         }
     }
 
     static async setUserEmail(value: string): Promise<void> {
-        if (client) {
-            return client.setUserEmail(value);
+        if (Crashlytics.client) {
+            return Crashlytics.client.setUserEmail(value);
         } else {
             logger.info(() => `No Fabric! set userEmail = ${value}`);
         }
